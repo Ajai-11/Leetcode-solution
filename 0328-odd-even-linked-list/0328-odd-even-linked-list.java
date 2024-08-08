d5bed35152ac07e2b669ec26957b74e22d3b9bc2
Ajai Kumar S R <144196235+Ajai-11@users.noreply.github.com>
2024-08-08 07:00:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+       ListNode odd=head,even = head.next,even_start = head.next;
+        while(even!=null && even.next!=null)
+        {
+            odd.next = odd.next.next; 
+            even.next = even.next.next; 
+            odd = odd.next; 
+            even = even.next;
+        }
+            odd.next = even_start; 
+            return head;  
+    }
+}
