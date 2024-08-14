89a6d5728d6d9acb5953c948f71e192d992d64bb
Ajai Kumar S R <144196235+Ajai-11@users.noreply.github.com>
2024-08-14 22:25:16 +0530

    Time: 59 ms (15.88%), Space: 49 MB (21.28%) - LeetHub

---
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const nums=[];
+    for (let i = 0; i < arr.length; i++) {
+        nums.push(fn(arr[i], i));
+    }
+
+    return nums;
+};
