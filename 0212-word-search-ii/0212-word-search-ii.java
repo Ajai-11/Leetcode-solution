0cda9eb2a3c7ffc3fbbea299964de48de07586af
Ajai Kumar S R <144196235+Ajai-11@users.noreply.github.com>
2024-08-17 10:48:17 +0530

    Time: 131 ms (81.93%), Space: 44.9 MB (58.79%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,34 +1,63 @@
 class Solution {
-     public boolean solve(char[][] board, int n, int m, int row, int col, String word, int i) {
-        if (i == word.length()) {
-            return true;
-        }
-        if (row < 0 || col < 0 || row >= n || col >= m || board[row][col] != word.charAt(i)) {
-            return false;
-        }
-        char temp = board[row][col];
-        board[row][col] = '*';
-        boolean res = solve(board, n, m, row - 1, col, word, i + 1) ||
-                      solve(board, n, m, row, col - 1, word, i + 1) ||
-                      solve(board, n, m, row + 1, col, word, i + 1) ||
-                      solve(board, n, m, row, col + 1, word, i + 1);
-        board[row][col] = temp;
-        return res;
+   class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word = null;
     }
+     private void insert(TrieNode root, String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.word = word;
+    }
+        
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> res=new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        TrieNode root = new TrieNode();
+        
+        
+        for (String word : words) {
+            insert(root, word);
+        }
+        
         int n = board.length;
         int m = board[0].length;
-         for(int k=0;k<words.length;k++){
-            boolean found = false;
-        for (int i = 0; i < n&& !found; i++) {
-            for (int j = 0; j < m&& !found; j++) {
-                if (solve(board, n, m, i, j, words[k], 0)) {
-                   res.add(words[k]);
-                   found=true;
-                }
+        
+        // Start DFS from each cell in the board
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dfs(board, i, j, root, result);
             }
-        }}
-        return res; 
+        }
+        
+        return result;
     }
+    
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> result) {
+        char c = board[i][j];
+        if (c == '#' || node.children[c - 'a'] == null) {
+            return;
+        }
+        
+        node = node.children[c - 'a'];
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null; 
+        }
+        
+        board[i][j] = '#';  
+        
+        if (i > 0) dfs(board, i - 1, j, node, result);
+        if (j > 0) dfs(board, i, j - 1, node, result);
+        if (i < board.length - 1) dfs(board, i + 1, j, node, result);
+        if (j < board[0].length - 1) dfs(board, i, j + 1, node, result);
+        
+        board[i][j] = c;  
+    }
+
+
 }
