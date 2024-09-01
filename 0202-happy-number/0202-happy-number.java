118aaaa653ec2ff0fbd54ee0c103679cc07901c1
Ajai Kumar S R <144196235+Ajai-11@users.noreply.github.com>
2024-09-01 06:53:45 +0530

    Time: 0 ms (100%), Space: 40.5 MB (76.51%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n>9){
+         int sum=0;
+         while(n!=0){
+          int rem=n%10;
+          sum=sum+(rem*rem);
+          n=n/10;
+         }
+         n=sum;
+       }
+        if(n==1||n==7)
+        return true;
+        else
+        return false;
+
+    }
+}
